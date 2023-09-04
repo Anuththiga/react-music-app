af1febc861dc3c0a5c528d1effd0e8968a189293
Tanuththiga <anuththiga25jt@gmail.com>
2023-09-04 15:53:12 +0800

    player components UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
+import Player from "./components/Player";
 
 function App() {
+  const [songs, setSongs] = useState([
+    {
+      title: "Jai Ho Slumdog Millionaire",
+      artist: "A.R.Rahman",
+      image: "./images/song-1.jpg",
+      src: "./music/Jai Ho.mp3"
+    },
+    {
+      title: "Jai Ho Slumdog Millionaire",
+      artist: "A.R.Rahman",
+      image: "./images/song-1.jpg",
+      src: "./music/Jai Ho.mp3"
+    }
+  ])
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+
   return (
     <div className="App">
-      
+      <Player
+        song = {songs[currentSongIndex]}
+        nextSong = {songs[nextSongIndex]}
+      />
     </div>
   );
 }
