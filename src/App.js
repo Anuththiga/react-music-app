ba8aa87e943acb1e16c094a0eb850ab438f71cb7
Tanuththiga <anuththiga25jt@gmail.com>
2023-09-05 13:17:55 +0800

    play, forward and backward functionalities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Player from "./components/Player";
 
 function App() {
-  const [songs, setSongs] = useState([
+  const [songs] = useState([
     {
       title: "Jai Ho Slumdog Millionaire",
       artist: "A.R.Rahman",
@@ -10,21 +10,34 @@ function App() {
       src: "./music/Jai Ho.mp3"
     },
     {
-      title: "Jai Ho Slumdog Millionaire",
-      artist: "A.R.Rahman",
-      image: "./images/song-1.jpg",
-      src: "./music/Jai Ho.mp3"
+      title: "Chogada",
+      artist: "Darshan Raval, Asees Kaur",
+      image: "./images/song-2.jpg",
+      src: "./music/Chogada.mp3"
     }
   ])
 
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if(currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex])
+
+
   return (
     <div className="App">
       <Player
-        song = {songs[currentSongIndex]}
-        nextSong = {songs[nextSongIndex]}
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        nextSongIndex={nextSongIndex}
+        songs={songs}
       />
     </div>
   );
--- src/components/PlayerControls.js
+++ src/components/PlayerControls.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faPause, faForward, faBackward } from '@fortawesome/free-solid-svg-icons';
 
-function PlayerControls() {
+function PlayerControls({isPlaying, setIsPlaying, moveSongForward, moveSongBackward}) {
   return (
     <div className='player-controls'>
-        <button className='move-btn'>
+        <button className='move-btn' onClick={moveSongBackward}>
             <FontAwesomeIcon icon={faBackward} />
         </button>
-        <button className='play-btn'>
-            <FontAwesomeIcon icon={faPlay} />
+        <button className='play-btn' onClick={()=> setIsPlaying(!isPlaying)}>
+            <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
         </button>
-        <button className='move-btn'>
+        <button className='move-btn' onClick={moveSongForward}>
             <FontAwesomeIcon icon={faForward} />
         </button>
     </div>
